cae9d250ba5f3c61a71667dab74a7c8ae00cd6f6
Sohan <sonu5650@gmail.com>
2020-04-09 06:33:23 +0000

    testing

--- config/auth.js
+++ config/auth.js
@@ -2,49 +2,10 @@
 
 // expose our config directly to our application using module.exports
 module.exports = {
-
-    facebookAuth: {
-        clientID: process.env.FACEBOOK_CLIENTID,
-        clientSecret: process.env.FACEBOOK_CLIENTSECRET,
-        callbackURL: process.env.DOMAIN + '/auth/facebook/callback',
-        profileURL: 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        profileFields: ['id', 'email', 'name'],
-		passReqToCallback: true
-    },
-
-    twitterAuth: {
-        consumerKey: process.env.TWITTER_CONSUMERKEY,
-        consumerSecret: process.env.TWITTER_CONSUMERSECRET,
-		callbackURL: process.env.DOMAIN + '/auth/twitter/callback',
-		passReqToCallback: true
-    },
-
-	googleAuth: {
-		clientID: process.env.GOOGLE_CLIENTID,
-		clientSecret: process.env.GOOGLE_CLIENTSECCRET,
-		callbackURL: process.env.DOMAIN + '/auth/google/callback',
-		passReqToCallback: true
-	},
-
 	githubAuth: {
-		clientID: process.env.GITHUB_CLIENTID,
-		clientSecret: process.env.GITHUB_CLIENTSECRET,
-		callbackURL: process.env.DOMAIN + '/auth/github/callback',
-		passReqToCallback: true
-	},
-
-	redditAuth: {
-		clientID: process.env.REDDIT_CLIENTID,
-		clientSecret: process.env.REDDIT_CLIENTSECRET,
-		callbackURL: process.env.DOMAIN + '/auth/reddit/callback',
-		passReqToCallback: true
-	},
-
-	discordAuth: {
-		clientID: process.env.DISCORD_CLIENTID,
-		clientSecret: process.env.DISCORD_CLIENTSECRET,
-		callbackURL: process.env.DOMAIN + '/auth/discord/callback',
-		scope: 'identify email',
+		clientID: 'Iv1.6e207d8c157ec748',
+		clientSecret: '0e1cddde4a5146d62aaa6df6223545de08b2e669',
+		callbackURL: 'http://nodes.bitcoinblack.net/auth/github/callback',
 		passReqToCallback: true
 	}
 
